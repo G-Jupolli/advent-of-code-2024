6f487e2355f2d293582aa6abad16ededc1a7bcaa
G-Jupolli <91795606+G-Jupolli@users.noreply.github.com>
2024-12-09 00:04:54 +0000

    Day 7 Part 2

--- go/src/day_7/day_7.go
+++ go/src/day_7/day_7.go
@@ -4,6 +4,7 @@ import (
 	"advent_of_code_2024/helpers"
 	"bufio"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -46,15 +47,11 @@ func DoDay7() (int, string, string) {
 			values = append(values, v_int)
 		}
 
-		if tryLine(target, values) {
+		if tryLine(target, values, false) {
 			part_1 += target
+		} else if tryLine(target, values, true) {
+			part_2 += target
 		}
-
-		// if p1_ok, line_val := checkLine(target, values); p1_ok {
-		// 	part_1 += line_val
-		// } else {
-		// 	part_2 += line_val
-		// }
 	}
 
 	part_2 += part_1
@@ -62,97 +59,29 @@ func DoDay7() (int, string, string) {
 	return 7, strconv.Itoa(part_1), strconv.Itoa(part_2)
 }
 
-func checkLine(target int, values []int) (bool, int) {
-
-	// The different combinations of + & * there can be
-	variations := (1 << (len(values) - 1))
-	// Start at 1 instead of 2 here as above validates if
-	// target is the sum of the values
-	checker := 0
-
-	for checker < variations {
-
-		accumilator := values[0]
-
-		for i := 1; i < len(values); i += 1 {
-			mul_flag := 1 << (i - 1)
-
-			if (mul_flag & checker) != 0 {
-				accumilator *= values[i]
-			} else {
-				accumilator += values[i]
-			}
-		}
-
-		if accumilator == target {
-			return true, target
-		}
-
-		checker += 1
-	}
-
-	return false, checkPart2(target, values)
-	// return false, 0
-}
-
-func checkPart2(target int, values []int) int {
-
-	// The different combinations of + & * there can be
-	variations := (1 << (len(values) - 1))
-	// Start at 1 instead of 2 here as above validates if
-	// target is the sum of the values
-	checker := 0
-
-	for checker < variations {
-		for x := 1; x < variations; x += 1 {
-
-			accumilator := values[0]
-
-			// fmt.Print(accumilator)
-
-			for i := 1; i < len(values); i += 1 {
-				curr_val := values[i]
-				flag_pos := 1 << (i - 1)
-
-				if (flag_pos & x) != 0 {
-					// print(" || ", curr_val)
-					accumilator = (accumilator * (len(strconv.Itoa(curr_val)) * 10)) + curr_val
-				} else if (flag_pos & checker) != 0 {
-					// print(" * ", curr_val)
-					accumilator *= curr_val
-				} else {
-					// print(" + ", curr_val)
-					accumilator += curr_val
-				}
-			}
-
-			// fmt.Print(" = ", accumilator)
-			// fmt.Println()
-
-			if accumilator == target {
-				return target
-			}
-
-		}
-		checker += 1
-	}
-
-	return 0
-}
-
-func tryLine(target int, values []int) bool {
+func tryLine(target int, values []int, allow_concat bool) bool {
 	if len(values) == 1 {
 		return target == values[0]
 	}
 
 	last_val := values[len(values)-1]
 
-	if target%last_val == 0 && tryLine(target/last_val, values[:len(values)-1]) {
+	if target%last_val == 0 && tryLine(target/last_val, values[:len(values)-1], allow_concat) {
 		return true
 	}
 
-	if target-last_val >= 0 && tryLine(target-last_val, values[:len(values)-1]) {
+	if target-last_val >= 0 && tryLine(target-last_val, values[:len(values)-1], allow_concat) {
 		return true
+	}
+
+	if allow_concat {
+
+		digit_len := len(strconv.Itoa(last_val))
+		ten_pow := int(math.Pow(float64(10), float64(digit_len)))
+
+		if target%ten_pow == last_val && tryLine(target/ten_pow, values[:len(values)-1], true) {
+			return true
+		}
 	}
 
 	return false
